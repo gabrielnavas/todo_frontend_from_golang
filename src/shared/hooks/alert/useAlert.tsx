972c64ddf1e9540fdb902d9462bbce17b3a26a66
navas <gabrielnavas@email.com>
2021-12-17 19:29:48 -0300

    create hook to alert messages

---
+++ src/shared/hooks/alert/useAlert.tsx
@@ -0,0 +1,35 @@
+import { toast } from 'react-toastify'
+
+type TypeAlert = 'error' | 'success' | 'warning' | 'error' | 'default'
+
+const useAlert = () => {
+  const handle = (type: TypeAlert, message: string) => {
+    const options = {
+      position: 'top-center',
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined
+    } as any
+    if (type === 'error') {
+      toast.info(message, options)
+    }
+    if (type === 'success') {
+      toast.success(message, options)
+    }
+    if (type === 'warning') {
+      toast.warning(message, options)
+    }
+    if (type === 'error') {
+      toast.error(message, options)
+    }
+    if (type === 'default') {
+      toast(message, options)
+    }
+  }
+  return { handle }
+}
+
+export { useAlert }
