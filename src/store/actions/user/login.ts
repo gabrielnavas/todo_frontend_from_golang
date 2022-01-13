5a59579286b89e7b807e584db8b7e2f1268fec8c
navas <gabrielnavas@email.com>
2022-01-13 06:07:20 -0300

    add logoff actions

--- src/store/actions/user/login.ts
+++ src/store/actions/user/login.ts
@@ -14,7 +14,24 @@ export const loginUserSuccess = (param: actionTypes.LoginUserSuccess) => ({
   payload: param
 })
 
+export const loginUserCredencialsWrong = (param: actionTypes.LoginUserCredencialsWrong) => ({
+  type: actionTypes.LOGIN_USER_CREDENCIALS_WRONG,
+  payload: param
+})
+
 export const loginUserFail = (param: actionTypes.FailParamDefault) => ({
   type: actionTypes.LOGIN_USER_FAIL,
   payload: param
+})
+
+export const logOffUserRequest = () => ({
+  type: actionTypes.LOGOFF_USER_REQUEST
+})
+
+export const logOffUserSuccess = () => ({
+  type: actionTypes.LOGOFF_USER_SUCCESS
+})
+
+export const logOffUserFail = () => ({
+  type: actionTypes.LOGOFF_USER_FAIL
 })
